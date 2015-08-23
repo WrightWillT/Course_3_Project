13b2513167c8209d9b291f300605eb5af5b0e909
Will Wright <wright.will.t@gmail.com>
2015-08-23 13:16:52 -0700

    i hate git

---
+++ run_Analysis.R
@@ -0,0 +1,83 @@
+# check to see if the project folder exists; if not, create it
+if(!file.exists("./course_3_project")){dir.create("./course_3_project")}
+
+# download the file
+fileUrl <- "http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./course_3_project/Dataset.zip")
+
+# because keeping track of the download date helps when troubleshooting
+dateDownloaded <- date()
+
+# unzip the file
+unzip(zipfile="./course_3_project/Dataset.zip",exdir="./course_3_projec")
+path_rf <- file.path("./course_3_project" , "UCI HAR Dataset")
+
+# for testing to make sure the files were unzipped properly
+        # files<-list.files(path_rf, recursive=TRUE)
+        # files 
+
+# Read the Activity Files
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+
+#Read the Subject files
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+
+# Read Fearures files
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+# Concatenate the data tables with some row bindings (one for each type of data)
+
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+# then, set names for the subject, activity, and features
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+        # for features, read the table and only name the second column
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+# Finally, to create the completed dataset, do two column binds; one for the
+# subject/activity and one to merge that with the features
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+# subset the feature names matching the pattern mean or std and subset those
+# selected names to the orignal Data, creating a modified version
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+# Next, we have to label everything
+# read the activityLabels
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
+# substitute the wonky provided names with ones that make sense
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+# test names
+        # names(Data)
+# Beautiful, now on to making a tidy dataset using plyr
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
+# make a readme
+library(knitr)
+knit("run_Analysis.R", output="codebook.md")
+
+
+
+
